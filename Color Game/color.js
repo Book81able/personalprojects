4007089986c21246876c08a6e2128b9a72a1fa58
book81able <ecobb81@gmail.com>
2018-07-26 17:15:24 -0700

    Some changes

--- Color Game/color.js
+++ Color Game/color.js
@@ -1,6 +1,7 @@
-var colors = ["red","blue"];
+var colors = ["red","blue","green","black","pink","yellow","purple"];
 var p1, p2;
 var ding;
+var time = 5;
 
 function preload(){
 	ding = loadSound("Ding-Sound-Effect.mp3");
@@ -9,23 +10,24 @@ function setup(){
 	var can = createCanvas(windowWidth,windowHeight);
   	can.parent("sketch-holder");
   	frameRate(1);
-  	fill(random(colors));
-		rect(0,0,width/2,height);
-		fill(random(colors));
-		rect(width/2,0,width,height);
+  	p1 = random(colors);
+		p2 = random(colors);
 		ding.play();
 }
 
 function draw(){
-	if(frameCount % 10 == 0){
-		fill(random(colors));
-		rect(0,0,width/2,height);
-		fill(random(colors));
-		rect(width/2,0,width,height);
+	if(frameCount % time == 0){
+		p1 = random(colors);
+		p2 = random(colors);
 		ding.play();
 	}
-	fill(0);
+	fill(p1);
+	rect(0,0,width/2,height);
+	fill(p2);
+	rect(width/2,0,width,height);
+	fill(255);
 	textSize(64);
 	text("E",width/4-32,64);
 	text ("J",width*3/4 - 32, 64);
+	text (time - frameCount % time,width/2-32,64);
 }
