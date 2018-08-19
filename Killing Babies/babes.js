baf2c4582d191f233ca5dbafcf408dbdaafadf86
book81able <ecobb81@gmail.com>
2018-08-19 15:25:49 -0700

    Bit of a failure

---
+++ Killing Babies/babes.js
@@ -0,0 +1,20 @@
+var data;
+var list;
+var elementList = [];
+
+function preload(){
+	data = loadJSON("Datas/sondheim.json");
+}
+
+function setup(){
+	list = data.list;
+	createCanvas(0,0);
+	for(var i = 0;i<list.length;i++){
+		elementList.push(createElement("p",list[i]));
+		elementList[i].mousePressed(changeRed);
+	}
+}
+
+function changeRed(){
+	elementList[2].remove();
+}
